c1ac0734be414a50a3949e787df3e5ff0fce7a71
RamyaShanthi1994 <60088909+RamyaShanthi1994@users.noreply.github.com>
2020-01-30 14:57:53 +0530

    Update RemoteDriver.java

--- src/practice/RemoteDriver.java
+++ src/practice/RemoteDriver.java
@@ -1,14 +1,14 @@
 package practice;
 
-import org.testng.annotations.Test;
+// import org.testng.annotations.Test;
 import java.net.MalformedURLException;
-import java.net.URL;
+// import java.net.URL;
 
-import org.openqa.selenium.Platform;
+// import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+// import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class RemoteDriver {
